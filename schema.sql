238c078c4b9d61b55065732b218c871b447ba8a2
lyanneagger <lyanneagger@gmail.com>
2021-06-01 18:05:02 -0400

    add queries and update schema

--- schema.sql
+++ schema.sql
@@ -43,7 +43,7 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (dept_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -53,7 +53,7 @@ CREATE TABLE titles (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * FROM departments;
